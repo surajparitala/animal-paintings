73e62acd5fbfb5c2711ff18becb572fed147d190
surajparitala <paritala.suraj@gmail.com>
2023-08-11 17:59:15 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductModule } from './product/product.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    CartModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-cart-view',
+  templateUrl: './cart-view.component.html',
+  styleUrls: ['./cart-view.component.css'],
+})
+export class CartViewComponent implements OnInit {
+  cartItems: Product[] = [];
+  totalPrice: number = 0;
+  constructor(private cartService: CartService) {}
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe((cartItems) => {
+      this.cartItems = cartItems;
+      this.totalPrice = this.getTotalPrice();
+    });
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+    for (let item of this.cartItems) {
+      total += item.price;
+    }
+    return total;
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart().subscribe();
+  }
+
+  checkOut(): void {
+    this.cartService.checkOut(this.cartItems).subscribe();
+  }
+}
